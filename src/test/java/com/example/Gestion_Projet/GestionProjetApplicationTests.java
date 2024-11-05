f9ceea39563edccbcaeb0af5e90d6e552a820f8f
aminelahlib <a.lahblib@itroadgroup.com>
2024-11-05 09:45:12 +0100

    new project

---
+++ src/test/java/com/example/Gestion_Projet/GestionProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gestion_Projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
